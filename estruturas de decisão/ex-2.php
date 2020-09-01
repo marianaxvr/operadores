c59383461620defa4d76dd4b9e169739e1e1c6c7
Mariana <mdxavier@live.com>
2020-09-01 14:25:59 -0300

    dia de feira

--- estruturas de decisão/ex-2.php
+++ estruturas de decisão/ex-2.php
@@ -1,7 +1,34 @@
 <?php
 //switch case
 $diaDaSemana = date("w");
-echo $diaDaSemana;
+// echo $diaDaSemana; -> vai me retornar o valor do dia da semana, abaixo o que o usuário final verá
+
+switch ($diaDaSemana) {
+    case 0:
+        echo "Domingo";
+    break;
+
+    case 1:
+        echo "Segunda-feira";
+    break;
+    // case 2, 3, em diante segue o mesmo padrão
+    case 2:
+        echo "Terça-feira";
+    break;
+    case 3:
+        echo "Quarta-feira";
+    break;
+    case 4:
+        echo "Quinta-feira";
+    break;
+    case 5:
+        echo "Sexta-feira";
+    break;
+    case 6:
+        echo "Sábado";
+    break;
+}
+// hoje é terça, ele me retornou terça-feira
 
 
 ?>
