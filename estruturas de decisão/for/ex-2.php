4fd4f4ec6e786c2e9b13a207d13f9b8b64be0ee9
Mariana <mdxavier@live.com>
2020-09-02 09:31:20 -0300

    select

--- estruturas de decisão/for/ex-2.php
+++ estruturas de decisão/for/ex-2.php
@@ -1,8 +1,10 @@
 <?php
-for($i=date("Y"); $i > date("Y")-100; $i--){
+echo "<select>";
 
+for($i=date("Y"); $i >= date("Y")-100; $i--){
+echo '<option value="'.$i.'">'.$i.'</option>';
 
 }
-
+echo "</select>";
 
 ?>
