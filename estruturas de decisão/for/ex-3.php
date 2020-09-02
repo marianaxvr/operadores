9ec7be706ebc6ed4fa8b758a4f4701793299fa01
Mariana <mdxavier@live.com>
2020-09-02 09:51:33 -0300

    foreach

--- estruturas de decisão/for/ex-3.php
+++ estruturas de decisão/for/ex-3.php
@@ -4,7 +4,11 @@ $meses = array(
     "Janeiro", "Fevereiro", "Março", 
     "Abril", "Maio", "Junho",
     "Julho", "Agosto", "Setembro",
-    "Outubro", "Novembro", "Dezembro";
+    "Outubro", "Novembro", "Dezembro"
 );
+foreach ($meses as $mes){
+echo "O mês é: ".$mes;
+
+}
 
 ?>
