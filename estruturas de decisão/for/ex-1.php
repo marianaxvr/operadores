31c98df29f0bf860af2fb8c78ea765a61fc81206
Mariana <mdxavier@live.com>
2020-09-02 09:23:30 -0300

    error

--- estruturas de decisão/for/ex-1.php
+++ estruturas de decisão/for/ex-1.php
@@ -5,7 +5,10 @@
     if ($ > 200 && $i < 800) continue; //ou<= ou >=
     if ($i === 900) break;
     echo $i . "<br>";
-
+// for para derrubar um servidor seria dessa forma: NÃO FAZER ISSO // se você erra a lógica, problema seu
+    // for ($i = 0; $i < 10; $i--) {
+        //   echo $i;  
+   // }
 
 }
 
